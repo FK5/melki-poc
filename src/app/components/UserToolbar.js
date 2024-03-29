f47c46505da623896606ed41110304c00fa84e95
Fouad Khalife <36138633+FK5@users.noreply.github.com>
2024-03-29 12:42:11 +0200

    fix user tool bar icons

--- src/app/components/UserToolbar.js
+++ src/app/components/UserToolbar.js
@@ -2,10 +2,9 @@ import { Badge } from "antd"
 
 import { CustomInput } from "./CustomComponents/CustomInput";
 
-import { CgProfile } from "react-icons/cg";
-import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
-
-import { FaRegBell } from "react-icons/fa6";
+import { PiChatDots } from "react-icons/pi";
+import { PiUserCircle } from "react-icons/pi";
+import { PiBell } from "react-icons/pi";
 
 import { primary } from "../variables/colors";
 
@@ -16,16 +15,16 @@ export const UserToolbar = () => {
             <div className="flex gap-2">
                 <div className="flex items-center justify-center rounded-full hover:bg-gray-200 h-8 w-8">
                     <Badge color={primary} count={5}>
-                        <IoChatbubbleEllipsesOutline className={"text-black"} size={20} />
+                        <PiChatDots className={"text-black"} size={20} />
                     </Badge>
                 </div>
                 <div className="flex items-center justify-center rounded-full hover:bg-gray-200 h-8 w-8">
                     <Badge color={primary} count={3}>
-                        <FaRegBell className={"text-black"} size={18} />
+                        <PiBell className={"text-black"} size={18} />
                     </Badge>
                 </div>
                 <div className="flex items-center justify-center rounded-full hover:bg-gray-200 h-8 w-8">
-                    <CgProfile className={"text-black"} size={20} />
+                    <PiUserCircle className={"text-black"} size={20} />
                 </div>
             </div>
         </>
